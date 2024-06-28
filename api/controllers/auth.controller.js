ec1335ff29d871547be84afe48ffbf4233cfdce4
DongChuyenNghiep <tuan22000@student.hamk.fi>
2024-06-28 17:24:57 +0700

    update small test

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -2,6 +2,7 @@ import User from '../models/user.model.js';
 import bcryptjs from 'bcryptjs';
 import { errorHandler } from '../utils/error.js';
 import jwt from 'jsonwebtoken';
+import Team from '../models/team.model.js';
 
 export const signup = async (req, res, next) => {
   const { riotID,username, email, password } = req.body;
@@ -10,6 +11,46 @@ export const signup = async (req, res, next) => {
   try {
     await newUser.save();
     res.status(201).json({ message: 'User created successfully' });
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const addteam = async (req, res, next) => {
+  const { team,shortname,player1,player2,player3,player4,player5,player6} = req.body;
+  const newTeam = new Team({ team,shortname,player1,player2,player3,player4,player5,player6 });
+  try {
+    await newTeam.save();
+    res.status(201).json({ message: 'Team created successfully' });
+  } catch (error) {
+    next(error);
+  }
+};
+export const findteam = async (req, res, next) => {
+  const { player} = req.body;
+  try {
+    const validUser = await Team.findOne({
+      $or: [
+        { player1: player },
+        { player2: player },
+        { player3: player },
+        { player4: player },
+        { player5: player },
+        { player6: player },
+        { player7: player },
+        { player8: player },
+        { player9: player },
+        { player10: player }
+      ]
+    });
+    if (!validUser) return next(errorHandler(404, 'User not found'));
+    const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
+    const { ...rest } = validUser._doc;
+    const expiryDate = new Date(Date.now() + 2629824000); // 1 hour
+    res
+      .cookie('access_token', token, { httpOnly: true, expires: expiryDate })
+      .status(200)
+      .json(rest);
   } catch (error) {
     next(error);
   }
